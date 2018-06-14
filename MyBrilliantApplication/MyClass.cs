0a80af0ea8b551077f84e7d84358df8ebbb29cec
Jose <quesjose1@gmail.com>
2018-06-14 08:09:44 -0400

    Add project files.

---
+++ MyBrilliantApplication/MyClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyBrilliantApplication{
+
+    public class MyClass{
+        
+        public string SayHello(){
+
+            string greeting = "Oh, hi there!";
+
+            return greeting;
+
+        }
+
+    }
+
+}
---
+++ myClass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+public class MyClass{
+	public MyClass(){
+		//
+		// TODO: Add constructor logic here
+		//
+	}
+}
